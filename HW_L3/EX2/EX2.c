034f162ab10b9362ba9ade0fcf28c061ce9e4bd8
YasmiiiinMustafa <92819440+YasmiiiinMustafa@users.noreply.github.com>
2021-10-24 23:53:27 +0200

    Add files via upload

---
+++ HW_L3/EX2/EX2.c
@@ -0,0 +1,17 @@
+/*
+ * EX2.c
+ *
+ *  Created on: Oct 24, 2021
+ *      Author: yasmine mostafa
+ */
+
+
+#include<stdio.h>
+
+int main()
+{
+	int x;
+	scanf("%d",&x);
+    printf("enter your integer :%d ",x);
+
+}
---
+++ HW_L3/EX4/EX4.c
@@ -0,0 +1,22 @@
+/*
+ * EX4.c
+ *
+ *  Created on: Oct 24, 2021
+ *      Author: yasmine mostafa
+ */
+
+
+#include<stdio.h>
+
+int main()
+{
+	float n1,n2;
+	float mul=0;
+	scanf(" %f %f",&n1,&n2);
+	printf("enter your float numbers : %f %f\n",n1,n2);
+
+	mul=n1*n2;
+
+	printf("mul= %f",mul);
+	return 0;
+}
