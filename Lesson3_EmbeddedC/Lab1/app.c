3a88d81c28590af4d3f3164a3db2bd9facea1464
YasmiiiinMustafa <92819440+YasmiiiinMustafa@users.noreply.github.com>
2022-02-15 07:00:31 +0200

    Delete Lesson3_EmbeddedC directory

--- Lesson3_EmbeddedC/Lab1/app.c
+++
@@ -1,9 +0,0 @@
-#include"uart.h"
-
-unsigned char string_buffer[100]= "learn-in-depth:yasmin";
-unsigned char const string_buffer2[100]= "learn-in-depth:yasmin";
-
-void main (void)
-{
-uart_send_string(string_buffer);
-}
